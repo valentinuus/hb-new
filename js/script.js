48fa7d82709a3fd306c828bf5775e87ad4526929
Valentin <72602932+valentinuus@users.noreply.github.com>
2024-03-21 06:43:08 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -16,24 +16,24 @@ testWebP(function (support) {
 // testWebP
 
 // бургер меню
-// const burger = document.querySelector('[data-burger]');
-// const nav = document.querySelector('[data-nav]');
-// const body = document.body;
-// const navItems = nav.querySelectorAll('a');
+const burger = document.querySelector('[data-burger]');
+const nav = document.querySelector('[data-nav]');
+const body = document.body;
+const navItems = nav.querySelectorAll('a');
 
-// burger.addEventListener('click', () => {
-// 	body.classList.toggle('stop--scroll')
-// 	burger.classList.toggle('burger--active');
-// 	nav.classList.toggle('nav--visible');
-// });
+burger.addEventListener('click', () => {
+	body.classList.toggle('stop--scroll')
+	burger.classList.toggle('burger--active');
+	nav.classList.toggle('nav--visible');
+});
 
-// navItems.forEach(el => {
-// 	el.addEventListener('click', () => {
-// 		body.classList.remove('stop--scroll')
-// 		burger.classList.remove('burger--active');
-// 		nav.classList.remove('nav--visible');
-// 	});
-// });
+navItems.forEach(el => {
+	el.addEventListener('click', () => {
+		body.classList.remove('stop--scroll')
+		burger.classList.remove('burger--active');
+		nav.classList.remove('nav--visible');
+	});
+});
 // бургер меню
 
 // // аккаордион
